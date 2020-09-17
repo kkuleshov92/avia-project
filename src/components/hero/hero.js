0571ba18d7b028ae6095ae4d58f78afa576b959c
kkuleshov <kkuleshov92@gmail.com>
2020-09-17 11:07:17 +0300

    back

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -4,3 +4,13 @@ $('.hero__list').slick({
     nextArrow: $('.hero__slider-btn_next'),
     dots: false
 })
+
+$('.hero__list').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+    let heroSection = document.querySelector('.hero');
+
+    heroSection.classList.remove('hero_active-slide_1');
+    heroSection.classList.remove('hero_active-slide_2');
+    heroSection.classList.remove('hero_active-slide_3');
+
+    heroSection.classList.add(`hero_active-slide_${nextSlide + 1}`);
+});
